1d140b10401358c1932d6ce51f6143f1b047780e
Rodrigo Teles <rtelesconsultor@gmail.com>
2025-03-14 12:16:10 -0300

    Treinamento em andamento

--- treinamento-1.js
+++ treinamento-1.js
@@ -2,3 +2,5 @@ console.log("Este treinamento esta ficando bom!")
 console.log("Segundo comando par teste!")
 
 console.log("dificulade de adicionar conteudo no js")
+
+console.log("Treinando minha espertase em Git")
